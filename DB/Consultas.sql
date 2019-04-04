96952b50de370f8c731c79f6ef143257e2ff0586
Alejandro Garcia Capella <wateralexpolo@gmail.com>
2019-04-04 09:57:57 +0200

    Consultas 1 terminadas , archivo nuevo consultas 2

--- DB/Consultas.sql
+++ DB/Consultas.sql
@@ -19,9 +19,31 @@ GROUP BY mercado.merccodi, mercado.mercdesc) ,
          AND estadoreserva.esrevisibilidad = 0
          AND reservas.esrecodi
         IN(2,7,9,14,16)
-        GROUP BY mercado.merccodi)
-SELECT tr.NombreMercado,tr.TotalReservas,tr.ImporteReservas, ca.ReservasCanceladas, ca.ImporteCanceladas FROM totalReservas tr INNER JOIN CANCELADOS ca ON tr.idMercado = ca.idMercado;
-
+        GROUP BY mercado.merccodi),
+    MEDIA AS
+    (select reservas.merccodi as CodigoMercado,sum(reservas.resefechafin - reservas.resefechaini) / count(reservas.resefechafin - reservas.resefechaini) as MediaNoches, 
+   sum(reservas.resenumadultos) / count(reservas.resenumadultos) as MediaAdultos, sum(reservas.resenumnins) / count(reservas.resenumnins) as MediaNiños
+   FROM volcp.reservas 
+    INNER JOIN volgc.mercado ON reservas.merccodi = mercado.merccodi
+    INNER JOIN volcp.estadoreserva ON estadoreserva.esrecodi = reservas.esrecodi
+    WHERE 
+        reservas.sogecodi = 1
+    AND mercado.mercficticio = 0
+    AND estadoreserva.esrevisibilidad = 0
+    GROUP BY reservas.merccodi),
+    HOTEL AS (SELECT reservas.merccodi as MercCodi ,STATS_MODE(reservas.centcodi) as CodigoHotel from volcp.reservas 
+        INNER JOIN volgc.mercado ON reservas.merccodi = mercado.merccodi
+        INNER JOIN volcp.estadoreserva ON estadoreserva.esrecodi = reservas.esrecodi
+        INNER JOIN volgc.centros ON reservas.centcodi = centros.centcodi
+    WHERE reservas.sogecodi = 1
+        AND mercado.mercficticio = 0
+        AND estadoreserva.esrevisibilidad = 0
+    GROUP BY reservas.merccodi)
+SELECT tr.NombreMercado,tr.TotalReservas,tr.ImporteReservas, ca.ReservasCanceladas, ca.ImporteCanceladas , me.MediaNoches, me.MediaAdultos,me.MediaNiños, ho.CodigoHotel
+FROM totalReservas tr 
+    INNER JOIN CANCELADOS ca ON tr.idMercado = ca.idMercado
+    INNER JOIN MEDIA me ON tr.idMercado = me.codigoMercado
+    INNER JOIN HOTEL ho ON tr.idMercado = ho.MercCodi;
 
 -- Total de reservas y su importe
 SELECT mercado.mercdesc,
@@ -45,14 +67,30 @@ GROUP BY reservas.merccodi, mercado.mercdesc;
         IN(2,7,9,14,16)
         GROUP BY reservas.merccodi;
         
--- Media de noches 
+-- Media de noches, adultos por habitacion y niños por habitacion
 
-   select (reservas.resefechafin - reservas.resefechaini)  FROM volcp.reservas, volgc.mercado, volcp.estadoreserva
-    WHERE volgc.mercado.merccodi = volcp.reservas.merccodi
-         AND volcp.estadoreserva.esrecodi = volcp.reservas.esrecodi
-         AND reservas.sogecodi = 1
-         AND mercado.mercficticio = 0
-         AND estadoreserva.esrevisibilidad = 0
-        GROUP BY reservas.merccodi;
-        
-        select (reservas.resefechafin - reservas.resefechaini) from volcp.reservas;
+   select reservas.merccodi as CodigoMercado,sum(reservas.resefechafin - reservas.resefechaini) / count(reservas.resefechafin - reservas.resefechaini) as MediaNoches, 
+   sum(reservas.resenumadultos) / count(reservas.resenumadultos) as MediaAdultos, sum(reservas.resenumnins) / count(reservas.resenumnins) as MediaNiños
+   FROM volcp.reservas 
+    INNER JOIN volgc.mercado ON reservas.merccodi = mercado.merccodi
+    INNER JOIN volcp.estadoreserva ON estadoreserva.esrecodi = reservas.esrecodi
+    WHERE 
+        reservas.sogecodi = 1
+    AND mercado.mercficticio = 0
+    AND estadoreserva.esrevisibilidad = 0
+    GROUP BY reservas.merccodi;
+
+    
+-- Hotel con mas reservas
+    SELECT reservas.merccodi as MercCodi ,STATS_MODE(reservas.centcodi) as CodigoHotel from volcp.reservas 
+        INNER JOIN volgc.mercado ON reservas.merccodi = mercado.merccodi
+        INNER JOIN volcp.estadoreserva ON estadoreserva.esrecodi = reservas.esrecodi
+        INNER JOIN volgc.centros ON reservas.centcodi = centros.centcodi
+    WHERE reservas.sogecodi = 1
+        AND mercado.mercficticio = 0
+        AND estadoreserva.esrevisibilidad = 0
+    GROUP BY reservas.merccodi;
+    
+ 
+
+   
